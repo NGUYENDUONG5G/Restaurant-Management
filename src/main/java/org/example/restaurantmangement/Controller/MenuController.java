5ec5030dd4a6033c1841df86dc6420d027c7ba80
NGUYENDUONG5G <apeach2901@gmail.com>
2025-07-26 17:23:22 +0700

    complete add food

--- src/main/java/org/example/restaurantmangement/Controller/MenuController.java
+++ src/main/java/org/example/restaurantmangement/Controller/MenuController.java
@@ -13,7 +13,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
@@ -50,6 +52,8 @@ public class MenuController implements Initializable {
     @FXML
     private TextField enterSearchFood;
     @FXML
+    private Button buttonSave;
+    @FXML
     private Button butonSearch;
     @FXML
     private Button buttonAddFood;
@@ -66,6 +70,8 @@ public class MenuController implements Initializable {
     @FXML
     private Button buttonUpdateFood;
     @FXML
+    private Button buttonChooseImage;
+    @FXML
     private ImageView logoRestaurant;
     @FXML
     private TableView<Food> tableFoods;
@@ -81,6 +87,8 @@ public class MenuController implements Initializable {
     private AnchorPane anchorListView;
     @FXML
     private ImageView imageFood;
+    @FXML
+    private TextField enterIdFood;
     @FXML
     private TextField enterNameFood;
     @FXML
@@ -242,6 +250,7 @@ public class MenuController implements Initializable {
         enterTypeFood.setVisible(content);
         enterPriceL.setVisible(content);
         enterPriceM.setVisible(content);
+        buttonChooseImage.setVisible(content);
         showNameFood.setVisible(!content);
         showTypeFood.setVisible(!content);
         showPriceM.setVisible(!content);
@@ -251,7 +260,68 @@ public class MenuController implements Initializable {
         lineFoodsTool.setVisible(!content);
     }
 
+    private boolean processSaveFood() {
+        boolean result = true;
+        String id = enterIdFood.getText();
+        String name = enterNameFood.getText();
+        String type = enterTypeFood.getText();
+        String priceM = enterPriceM.getText();
+        String priceL = enterPriceL.getText();
 
+        if (!id.isBlank()) {
+            enterIdFood.setStyle("-fx-border-color: transparent");
+        } else {
+            enterIdFood.setStyle("-fx-border-color: red");
+
+        }
+        if (!name.isBlank()) {
+            enterNameFood.setStyle("-fx-border-color: transparent");
+        } else {
+            enterNameFood.setStyle("-fx-border-color: red");
+        }
+
+        if (!type.isBlank()) {
+            enterTypeFood.setStyle("-fx-border-color: transparent");
+        } else {
+            enterTypeFood.setStyle("-fx-border-color: red");
+        }
+        if (selectedFile != null) {
+            buttonChooseImage.setStyle("-fx-border-color: transparent");
+        } else {
+            buttonChooseImage.setStyle("-fx-border-color: red");
+        }
+        HashMap<String, Long> prices = new HashMap<>();
+        try {
+            if (!priceM.isBlank()) {
+
+                prices.put("M", Long.parseLong(priceM));
+                enterPriceM.setStyle("-fx-border-color: transparent");
+            } else {
+                enterPriceM.setStyle("-fx-border-color: red");
+            }
+        } catch (NumberFormatException e) {
+            enterPriceM.setStyle("-fx-border-color: red");
+        }
+        try {
+            if (!priceL.isBlank()) {
+
+                prices.put("L", Long.parseLong(priceL));
+                enterPriceL.setStyle("-fx-border-color: transparent");
+            } else {
+                enterPriceL.setStyle("-fx-border-color: red");
+            }
+        } catch (NumberFormatException e) {
+            enterPriceL.setStyle("-fx-border-color: red");
+        }
+        if (!name.isBlank() && !type.isBlank() && !priceM.isBlank() && !priceL.isBlank()) {
+            Menu.setInsert(id, name, type, prices, selectedFile);
+            tableFoods.refresh();
+            setFoodsList(null);
+        } else {
+            result = false;
+        }
+        return result;
+    }
 
     private void processUpdateFood() {
         String id = showIdFood.getText();
@@ -320,8 +390,13 @@ public class MenuController implements Initializable {
         } else {
             foodList = foodList_;
         }
-
         tableFoods.setItems(foodList);
+    }
+
+    private void setVisibleId(boolean content) {
+        showIdFood.setVisible(content);
+        enterIdFood.setVisible(!content);
+        buttonSave.setVisible(!content);
     }
 
     private void processChooseImage() {
@@ -332,6 +407,27 @@ public class MenuController implements Initializable {
         );
         selectedFile = fileChooser.showOpenDialog((Stage) tableFoods.getScene().getWindow());
 
+    }
+
+    @FXML
+    void actionChooseImage(MouseEvent event) {
+        if (event.getSource() == buttonChooseImage) {
+            processChooseImage();
+            if (selectedFile != null) {
+                imageFood.setImage(new Image(selectedFile.toURI().toString()));
+            }
+        }
+    }
+
+    @FXML
+    void actionSaveInfo(MouseEvent event) {
+        if (event.getSource() == buttonSave) {
+            if (showConfirm("Are you sure you want to save?")) {
+                if (processSaveFood()) {
+                    setVisiblePane(anchorFoods);
+                }
+            }
+        }
     }
 
     @FXML
@@ -363,10 +459,21 @@ public class MenuController implements Initializable {
             setVisiblePane(anchorDetailFood);
             lineDetailTool.setVisible(true);
             setAcceptEdit(true);
+            setVisibleId(true);
 
 
         } else if (event.getSource() == buttonAddFood) {
-
+            enterIdFood.clear();
+            enterNameFood.clear();
+            enterTypeFood.clear();
+            enterPriceM.clear();
+            enterPriceL.clear();
+            imageFood.setImage(null);
+            setVisiblePane(anchorDetailFood);
+            setAcceptEdit(true);
+            buttonChooseImage.setVisible(true);
+            setVisibleId(false);
+            lineDetailTool.setVisible(false);
         } else if (event.getSource() == buttonDeleteFood) {
             if (showConfirm("Are you sure you want to delete this item?")) {
                 Menu.setDelete(selectedFood.getId());
@@ -416,6 +523,7 @@ public class MenuController implements Initializable {
                     setVisiblePane(anchorDetailFood);
                     lineFoodsTool.setVisible(true);
                     processSelectedFood();
+                    setVisibleId(true);
                 } else if (!row.isEmpty() && event.getClickCount() == 1) {
                     lineFoodsTool.setVisible(true);
                     selectedFood = row.getItem();
@@ -459,6 +567,7 @@ public class MenuController implements Initializable {
             if (selectedItem != null) {
                 selectedFood = Menu.getFoods().get(splitStr[0]);
                 setVisiblePane(anchorDetailFood);
+                setVisibleId(true);
                 lineFoodsTool.setVisible(true);
                 processSelectedFood();
             }
